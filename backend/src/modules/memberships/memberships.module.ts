e47399514a58cb1c757029da05ee31fb8079cc76
lucasabella <lucasagustinabella@gmail.com>
2024-05-08 17:36:33 -0300

    membership crud and seeder rdy

--- backend/src/modules/memberships/memberships.module.ts
+++ backend/src/modules/memberships/memberships.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MembershipsService } from './memberships.service';
 import { MembershipsController } from './memberships.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Membership } from 'src/entities/membership.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Membership])],
   controllers: [MembershipsController],
   providers: [MembershipsService],
 })
--- backend/src/modules/memberships/memberships.service.ts
+++ backend/src/modules/memberships/memberships.service.ts
@@ -1,16 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Membership } from '../../entities/membership.entity';
+import { Repository } from 'typeorm';
+// import * as membershipsData from '../../helpers/memberships.json';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const data = require('../../helpers/memberships.json');
 
 @Injectable()
 export class MembershipsService {
-  findAll() {
-    return `This action returns all memberships`;
+  constructor(
+    @InjectRepository(Membership)
+    private membershipRepository: Repository<Membership>,
+  ) {}
+
+  async getMemberships() {
+    try {
+      const memberships = await this.membershipRepository.find();
+      return memberships;
+    } catch (err) {
+      throw new NotFoundException('Memberships not found');
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} membership`;
+  async addMemberships() {
+    const memberships = data.map((membershipData) => {
+      const membership = new Membership();
+      membership.name = membershipData.name;
+      membership.price = membershipData.price;
+      membership.description = membershipData.description;
+      membership.duration = membershipData.duration;
+      return membership;
+    });
+    try {
+      await this.membershipRepository.save(memberships);
+      return 'Memberships preloaded';
+    } catch (err) {
+      throw new BadRequestException('Failed to seed memberships');
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} membership`;
+  async getMembershipById(id: string): Promise<Membership> {
+    const membership = await this.membershipRepository.findOne({
+      where: { id },
+    });
+    if (!membership) {
+      throw new NotFoundException('Membership not found');
+    }
+    return membership;
   }
 }
