040ecef039677fd201cc7dfa1797649631350b7c
MateoGaviraghi <mateogaviraghi24@gmail.com>
2024-05-09 17:53:54 -0300

    cloudinary

---
+++ backend/src/files/files.controller.ts
@@ -0,0 +1,39 @@
+import {
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  Param,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { FilesService } from './files.service';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('uploadImage/:id')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadImage(
+    @Param('id') userId: string,
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({
+            maxSize: 2000000,
+            message: 'The image must be less than 2MB',
+          }),
+          new FileTypeValidator({
+            fileType: /(jpg|jpeg|png|webp|gif)$/i,
+          }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    return await this.filesService.uploadImage(userId, file);
+  }
+}
