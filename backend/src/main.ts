03573bb4f95a716b28b95497cff3dc0ee9a63dd6
AgustinN. Bertagna <agus2713@hotmail.com>
2024-05-09 14:42:15 -0300

    Implementacion de Cors con NestJS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,12 +3,9 @@ import { AppModule } from './app.module';
 import { Logger } from './middlewares/logger.middleware';
 import { Validation } from './pipes/validation.pipe';
 
-// import cors from 'cors';
-
-const cors = require('cors');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors());
+  app.enableCors();
   app.use(new Logger().use);
   app.useGlobalPipes(Validation);
   await app.listen(3000);
