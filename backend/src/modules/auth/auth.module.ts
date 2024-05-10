4a8ffd786fd95df987bcfd2ccddd3b628bba19f9
AgustinN. Bertagna <agus2713@hotmail.com>
2024-05-09 21:16:44 -0300

    Implementacion del servicio de mails en el modulo de auth

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
+import { EmailsModule } from '../emails/emails.module';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [UsersModule],
+  imports: [UsersModule, EmailsModule],
 })
 export class AuthModule {}
