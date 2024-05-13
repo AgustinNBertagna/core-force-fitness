a9423ad5e8661246f95009279ec6391b55544f47
AgustinN. Bertagna <agus2713@hotmail.com>
2024-05-13 18:33:27 -0300

    Importacion del servicio de Membresias en el modulo de usuarios

--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -4,9 +4,15 @@ import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { UserRepository } from './users.repository';
+import { MembershipsModule } from '../memberships/memberships.module';
+import { Membership } from 'src/entities/membership.entity';
+import { UserMemberships } from 'src/entities/userMembership.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User, Membership, UserMemberships]),
+    MembershipsModule,
+  ],
   controllers: [UsersController],
   providers: [UsersService, UserRepository],
   exports: [UserRepository],
