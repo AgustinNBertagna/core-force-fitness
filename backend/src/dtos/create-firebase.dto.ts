8eb9145cf34dc043af579b14ee5df918ff92cb3a
lucasabella <lucasagustinabella@gmail.com>
2024-05-16 15:39:51 -0300

    change on entity

--- backend/src/dtos/create-firebase.dto.ts
+++ backend/src/dtos/create-firebase.dto.ts
@@ -1,11 +1,4 @@
-import {
-  IsString,
-  MinLength,
-  MaxLength,
-  Matches,
-  IsNotEmpty,
-  IsEmail,
-} from 'class-validator';
+import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
 
 export class CreateFirebaseDto {
   @IsNotEmpty()
@@ -22,20 +15,4 @@ export class CreateFirebaseDto {
   @IsNotEmpty()
   @IsEmail({}, { message: 'Invalid email' })
   email: string;
-
-  @IsString()
-  @MinLength(8, { message: 'La contraseña debe tener al menos 8 caracteres.' })
-  @MaxLength(30, {
-    message: 'La contraseña no puede exceder los 30 caracteres.',
-  })
-  @Matches(/(?=.*[A-Z])/, {
-    message: 'La contraseña debe contener al menos una letra mayúscula.',
-  })
-  @Matches(/(?=.*[a-z])/, {
-    message: 'La contraseña debe contener al menos una letra minúscula.',
-  })
-  @Matches(/(?=.*[0-9])/, {
-    message: 'La contraseña debe contener al menos un número.',
-  })
-  password: string;
 }
