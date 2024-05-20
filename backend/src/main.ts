db69ed7684dffe14b5bdd45a82c62c4448a516be
AgustinN. Bertagna <agus2713@hotmail.com>
2024-05-20 10:29:07 -0300

    Hotfix port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.enableCors();
   app.use(new Logger().use);
   app.useGlobalPipes(Validation);
-  await app.listen(3000);
+  await app.listen(process.env.PORT as unknown as number);
 }
 bootstrap();
