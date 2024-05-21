98ec3df92672eca06785af8c061895f052baab95
dylan correa <D.correa.E05@gmail.com>
2024-05-21 15:12:51 -0300

    HotFix Lucas

--- backend/src/modules/files/files.module.ts
+++ backend/src/modules/files/files.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { cloudinaryConfig } from 'src/config/cloudinary';
 import { FilesRepository } from './files.repository';
+import { Routine } from 'src/entities/routines.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User,Routine])],
   controllers: [FilesController],
   providers: [FilesService, cloudinaryConfig, FilesRepository],
 })
