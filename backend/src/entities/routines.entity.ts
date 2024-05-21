50f37ddf45dc0f832c592fa2ecb244598fc38162
lucasabella <lucasagustinabella@gmail.com>
2024-05-21 11:31:03 -0300

    routines entity

---
+++ backend/src/entities/routines.entity.ts
@@ -0,0 +1,22 @@
+import { TypeRoutine } from 'src/helpers/routines.enum';
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity({
+  name: 'routines',
+})
+export class Routine {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  name: string;
+
+  @Column({ type: 'enum', enum: TypeRoutine, nullable: false })
+  type: TypeRoutine;
+
+  @Column({
+    type: 'varchar',
+    nullable: true,
+  })
+  pdf_url: string;
+}
---
+++ backend/src/helpers/routines.enum.ts
@@ -0,0 +1,6 @@
+export enum TypeRoutine {
+  CARDIO = 'cardio',
+  STRENGTH = 'strength',
+  FLEXIBILITY = 'flexibility',
+  HIIT = 'hiit',
+}
