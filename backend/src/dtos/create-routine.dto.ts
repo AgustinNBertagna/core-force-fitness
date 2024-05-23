92c14bc9c258ae34ebc3fa453bdfb1deecde4816
lucasabella <lucasagustinabella@gmail.com>
2024-05-23 09:38:14 -0300

    hot fixed dto

--- backend/src/dtos/create-routine.dto.ts
+++ backend/src/dtos/create-routine.dto.ts
@@ -1,10 +1,12 @@
-import { IsEnum, IsString } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
 import { TypeRoutine } from 'src/helpers/routines.enum';
 
 export class CreateRoutineDto {
+  @IsNotEmpty()
   @IsEnum(TypeRoutine)
   typeRoutine: TypeRoutine;
 
+  @IsNotEmpty()
   @IsString()
   routineName: string;
 }
