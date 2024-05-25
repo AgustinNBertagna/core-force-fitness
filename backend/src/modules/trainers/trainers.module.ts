1e55875775ad93f16c403eb9c1d7932f16da93e5
lucasabella <lucasagustinabella@gmail.com>
2024-05-25 17:13:48 -0300

    hotfix routine

--- backend/src/modules/trainers/trainers.module.ts
+++ backend/src/modules/trainers/trainers.module.ts
@@ -4,9 +4,10 @@ import { TrainersController } from './trainers.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { Routine } from 'src/entities/routines.entity';
+import { UsersRoutines } from 'src/entities/userRoutine.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Routine])],
+  imports: [TypeOrmModule.forFeature([User, Routine, UsersRoutines])],
   controllers: [TrainersController],
   providers: [TrainersService],
 })
