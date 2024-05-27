e5cb28a63e9b1ab223c0779aff88d4c1b51f2e9a
AgustinN. Bertagna <agus2713@hotmail.com>
2024-05-27 13:41:01 -0300

    Implementacion Mail de pricing

--- backend/src/modules/emails/emails.module.ts
+++ backend/src/modules/emails/emails.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EmailsService } from './emails.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [EmailsService],
   exports: [EmailsService],
 })
